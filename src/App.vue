84f974bf0dd8cc1da6ed4badc79d8aa1af1a286e
Ken Paulsen <ken.paulsen@gmail.com>
2020-09-10 20:16:14 +0200

    Set up basic DOM structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <form role="search">
+      <div class="search-component">
+        <input
+          type="search"
+          placeholder="Search"
+          aria-label="Hurtigruten ship search"
+          class="search-component__input"
+        >
+        <button
+          type="submit"
+          class="search-component__button"
+        >
+          <img
+            src="assets/magnifying-glass.svg"
+            alt="Search icon"
+          >
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
